be547e93e1f52f867c45e3e46ad387ab48d5960e
RajukumarK2000 <kasarapurajukumar04@gmail.com>
2025-04-11 15:46:06 +0530

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { HomePage } from './Pages/HomePage/HomePage';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./HomePage.css";
+
+export const HomePage = () => {
+  return (
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quibusdam quos libero repellat, voluptas nobis pariatur, quas accusantium soluta, error culpa. Sit voluptate ea dolor iure beatae quam repellendus voluptatem..</div>
+  )
+}
