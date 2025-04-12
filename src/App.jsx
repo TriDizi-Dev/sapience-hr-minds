8f23b378c9d43ed24e94a601b5920664f59f7283
RajukumarK2000 <kasarapurajukumar04@gmail.com>
2025-04-12 10:54:38 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './Pages/HomePage/HomePage';
+import { AboutPage } from './Pages/AboutPage/AboutPage';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/about" element={<AboutPage />}></Route>
     </Routes>
   </BrowserRouter>
   )
