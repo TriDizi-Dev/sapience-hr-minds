f81fa332e3a75b4b96dcb66ba837fa16c788c216
Manikanta1006 <chellurimanikanta989@gmail.com>
2025-04-16 12:54:28 +0530

    adf

--- src/Components/SingleBlogPage/SingleBlogPage.jsx
+++ src/Components/SingleBlogPage/SingleBlogPage.jsx
@@ -4,6 +4,9 @@ import bannerimg from "../../assets/SingleBlog/blogbanner.jpg"
 import blogimg1 from "../../assets/Blogs/blog1.svg"
 import blogimg2 from "../../assets/Blogs/blog2.svg"
 
+import background from "../../assets/SingleBlog/bbg2.svg"
+import triangle from "../../assets/SingleBlog/triangle.svg"
+
 function SingleBlogPage() {
 
      const blogs = [
@@ -97,7 +100,9 @@ function SingleBlogPage() {
 
         </div>
         <div className="single_blog_layer2_outer">
+
             <div className="single_blog_content">
+                <img src={triangle} alt="triangle" className="single_blog_triangle_icon" />
                 <h1 className="single_blog_content_heading">
                 Lorem ipsum dolor 
                 </h1>
@@ -145,7 +150,7 @@ function SingleBlogPage() {
         </div>
 
         <div className="single_blog_layer3_outer">
-
+            <img src={background} alt="background" className="single_blog_bg_img" />
             <div className="single_blog_layer3_main">
 
            
