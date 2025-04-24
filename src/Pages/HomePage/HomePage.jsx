7d1a73f44c87831df59b61cea46e98776276081a
RajukumarK2000 <kasarapurajukumar04@gmail.com>
2025-04-24 11:36:30 +0530

    Chnages

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -251,7 +251,6 @@ export const HomePage = () => {
 
     try {
       const formPayload = new FormData();
-      formPayload.append("sheet", "Sheet1");
       for (let key in formData) {
         formPayload.append(key, formData[key]);
       }
